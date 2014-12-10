428d3e10f62b7354cbcc27659f0404a160c36253
jasonpaulwong <jpw007@ucsd.edu>
2014-12-09 21:44:54 -0800

    Initial commit

---
+++ GameKitHelper.swift
@@ -0,0 +1,206 @@
+//
+//  GameKitHelper.swift
+//  CatRaceStarter
+//
+//  Created by Jason Wong on 11/11/14.
+//  Copyright (c) 2014 Raywenderlich. All rights reserved.
+//
+
+import GameKit
+import Foundation
+
+let PresentAuthenticationViewController : NSString = "present_authentication_view_controller"
+let LocalPlayerIsAuthenticated : NSString = "local_player_authenticated"
+    
+/* For singleton pattern */
+private let _GameKitHelperSharedInstace = GameKitHelper()
+
+
+protocol GameKitHelperDelegate {
+    func matchStarted()
+    func matchEnded()
+    func match(match:GKMatch, didReceiveData data: NSData!, fromPlayer playerID: NSString!)
+}
+
+class GameKitHelper : NSObject, GKMatchmakerViewControllerDelegate, GKMatchDelegate {
+    var _enableGameCenter : Bool
+    var _matchStarted : Bool
+    var _match : GKMatch!
+    var _delegate : GameKitHelperDelegate?
+    var authenticationViewController: UIViewController?
+    var lastError : NSError?
+    var playersDict : NSMutableDictionary?
+
+    /* For singleton pattern */
+    class var SharedGameKitHelper:GameKitHelper {
+        return _GameKitHelperSharedInstace
+    }
+    
+    override init() {
+        self._enableGameCenter = true
+        self._matchStarted = false
+        super.init()
+    }
+    
+    func authenticateLocalPlayer() {
+
+        var localPlayer = GKLocalPlayer.localPlayer()
+        
+        if(localPlayer.authenticated) {
+            NSNotificationCenter.defaultCenter().postNotificationName(LocalPlayerIsAuthenticated, object:nil)
+            return
+        }
+        
+        localPlayer.authenticateHandler = {(viewController : UIViewController!, error : NSError!) -> Void in
+
+            if(error != nil) {
+                self.setLastError(error)
+            }
+
+            if(viewController != nil) {
+                self.setAuthenticationViewController(viewController)
+            }
+            else if(GKLocalPlayer.localPlayer().authenticated) {
+                self._enableGameCenter = true
+                    NSNotificationCenter.defaultCenter().postNotificationName(LocalPlayerIsAuthenticated, object: nil)
+            }
+            else {
+                self._enableGameCenter = false
+            }
+            
+        }
+
+    }
+    
+    func setAuthenticationViewController(authViewController:UIViewController!) {
+        if(authViewController != nil) {
+            authenticationViewController = authViewController
+            NSNotificationCenter.defaultCenter().postNotificationName(PresentAuthenticationViewController, object:self)
+        }
+
+    }
+    
+    func setLastError(error: NSError) {
+
+        lastError = error.copy() as? NSError
+        
+        if((lastError) != nil) {
+            NSLog("GamerKitHelp ERROR: \(lastError?.userInfo?.description)")
+        }
+
+    }
+
+    
+    func findMatchWithMinPlayers(minPlayers:Int, maxPlayers:Int, viewController:UIViewController, delegate:GameKitHelperDelegate) {
+
+        if(!_enableGameCenter) {
+           return;
+        }
+
+        _matchStarted = false
+        self._match = nil
+        _delegate = delegate
+        viewController.dismissViewControllerAnimated(false, completion: nil)
+
+        let request = GKMatchRequest()
+        request.minPlayers = minPlayers
+        request.maxPlayers = maxPlayers
+
+        let mmvc = GKMatchmakerViewController(matchRequest: request)
+        mmvc.matchmakerDelegate = self
+        viewController.presentViewController(mmvc, animated: true, completion: nil)
+    }
+    
+    func lookupPlayers() {
+        println("Looking up \(_match.playerIDs.count) players...")
+
+        GKPlayer.loadPlayersForIdentifiers(_match?.playerIDs) { (players, error) -> Void in
+            if error != nil {
+                println("Error retrieving player info: \(error.localizedDescription)")
+                self._matchStarted = false
+                self._delegate?.matchEnded()
+            }
+            else {
+                self.playersDict = NSMutableDictionary(capacity: players.count)
+                for player in players {
+                    println("Found player: \(player.alias)")
+                    self.playersDict?.setObject(player, forKey: player.playerID)
+                }
+            }
+            self.playersDict?.setObject(GKLocalPlayer.localPlayer(), forKey: GKLocalPlayer.localPlayer().playerID)
+            self._matchStarted = true
+            self._delegate?.matchStarted()
+        }
+    }
+    
+    
+    /* For protocol GKMatchmakerViewControllerDelegate */
+    func matchmakerViewControllerWasCancelled(viewController:GKMatchmakerViewController) {
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func matchmakerViewController(viewController: GKMatchmakerViewController!, didFailWithError error:NSError!) {
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+        NSLog("Error finding match: %@", error.localizedDescription)
+    }
+    
+    func matchmakerViewController(viewController: GKMatchmakerViewController!,
+        didFindMatch match: GKMatch!) {
+
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+        self._match = match
+        match.delegate = self
+        if(!_matchStarted && match.expectedPlayerCount==0) {
+            NSLog("Ready to start match")
+            self.lookupPlayers()
+        }
+    }
+    
+    /* For protocol GKMatchDelegate */
+    func match(match: GKMatch!, didReceiveData data: NSData!, fromPlayer playerID: NSString!) {
+        if(_match != match) {
+            return
+        }
+        _delegate?.match(match, didReceiveData: data, fromPlayer: playerID)
+    }
+    
+    func match(match: GKMatch!,  player: String!, didChangeState state: GKPlayerConnectionState) {
+
+        if(_match != match) {
+            return
+        }
+        switch(state) {
+            case GKPlayerConnectionState.StateConnected:
+                if(!_matchStarted && match.expectedPlayerCount == 0) {
+                    NSLog("Ready to start match!")
+                    self.lookupPlayers()
+                }
+            case GKPlayerConnectionState.StateDisconnected:
+                NSLog("Player disconnected!")
+                _matchStarted = false
+                _delegate?.matchEnded()
+            default:
+                break
+        }
+    }
+    
+    func match(match: GKMatch!, connectionWithPlayerFailed:String!, withError error:NSError!) {
+        if(_match != match) {
+            return
+        }
+        NSLog("Failed to connect to player with error: %@", error.localizedDescription)
+        _matchStarted = false
+        _delegate?.matchEnded()
+
+    }
+    
+    func match(match: GKMatch!, didFailWithError error: NSError!) {
+        if(_match != match) {
+            return
+        }
+        NSLog("Match failed with error: %@", error.localizedDescription)
+        _matchStarted = false
+        _delegate?.matchEnded()
+    }
+}
---
+++ MultiplayerNetworking.swift
@@ -0,0 +1,268 @@
+//
+//  MultiplayerNetworking.swift
+//  CatRaceStarter
+//
+//  Created by Jason Wong on 11/20/14.
+//  Copyright (c) 2014 Raywenderlich. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+let playerIDKey = "PlayerID"
+let randomNumberKey = "randomNumber"
+
+protocol MultiplayerNetworkingProtocol {
+    func matchEnded()
+    func setCurrentPlayerIndex(index : Int)
+    func movePlayerAtIndex(index : Int)
+    func gameOver(player1Won : Bool)
+}
+
+enum MessageType : Int {
+    case kMessageTypeRandomNumber = 0
+    case kMessageTypeGameBegin
+    case kMessageTypeMove
+    case kMessageTypeGameOver
+}
+
+enum GameState : Int {
+    case kGameStateWaitingForMatch = 0
+    case kGameStateWaitingForRandomNumber
+    case kGameStateWaitingForStart
+    case kGameStateActive
+    case kGameStateDone
+}
+
+struct Message {
+    var messageType : MessageType
+}
+
+struct MessageRandomNumber {
+    var message : Message
+    var randomNumber : UInt32
+}
+
+struct MessageGameBegin {
+    var message : Message
+}
+
+struct MessageMove {
+    var message : Message
+}
+
+struct MessageGameOver {
+    var message : Message
+    var player1Won : Bool
+}
+
+
+class MultiplayerNetworking : NSObject, GameKitHelperDelegate {
+    var delegate : MultiplayerNetworkingProtocol?
+    var _ourRandomNumber : UInt32!
+    var _gameState : GameState?
+    var _isPlayer1 = false
+    var _receivedAllRandomNumbers = false
+    var _orderOfPlayers : NSMutableArray!
+
+    override init() {
+        super.init()
+        
+        _ourRandomNumber = arc4random()
+        _gameState = GameState.kGameStateWaitingForMatch
+        _orderOfPlayers = NSMutableArray()
+        
+        // Create a dictionary and add it to the orderOfPlayers array
+        var dict = ["\(playerIDKey)":"\(GKLocalPlayer.localPlayer().playerID)", "\(randomNumberKey)":"\(_ourRandomNumber)"]
+        _orderOfPlayers.addObject(dict)
+    }
+    
+    
+    func sendRandomNumber() {
+        var message = Message(messageType: MessageType.kMessageTypeRandomNumber)
+        var MessageRandomNum =  MessageRandomNumber(message: message, randomNumber: _ourRandomNumber!)
+        let data = NSData(bytes: &MessageRandomNum, length: sizeof(MessageRandomNumber))
+        self.sendData(data)
+    }
+    
+    func sendGameBegin() {
+        var message = Message(messageType: MessageType.kMessageTypeGameBegin)
+        var MessageGameBeg = MessageGameBegin(message: message)
+        var data = NSData(bytes: &MessageGameBeg, length: sizeof(MessageGameBegin))
+        self.sendData(data)
+    }
+    
+    func sendGameEnd(player1Won : Bool) {
+        var message = Message(messageType: MessageType.kMessageTypeGameOver)
+        var messageGameOver = MessageGameOver(message: message, player1Won: player1Won)
+        var data = NSData(bytes: &messageGameOver, length: sizeof(MessageGameOver))
+        self.sendData(data)
+    }
+    
+    func sendMove() {
+        var message = Message(messageType: MessageType.kMessageTypeMove)
+        var messageMove = MessageMove(message: message)
+        var data = NSData(bytes: &messageMove, length: sizeof(MessageMove))
+        self.sendData(data)
+    }
+    
+    func sendData(data : NSData) {
+        var error : NSError?
+        
+        let gameKitHelper = GameKitHelper.SharedGameKitHelper
+        var success = gameKitHelper._match.sendDataToAllPlayers(data, withDataMode: GKMatchSendDataMode.Reliable, error: &error)
+        /*CHECK HERE IF ERROR */
+        if(success == false) {
+            println("Error sending data: \(error?.localizedDescription)")
+            self.matchEnded()
+        }
+    }
+    
+    func indexForLocalPlayer() -> Int {
+        var playerID = GKLocalPlayer.localPlayer().playerID
+        return self.indexForPlayerWithId(playerID)
+    }
+    func indexForPlayerWithId(playerId : String) -> Int {
+        var index = -1
+        _orderOfPlayers.enumerateObjectsUsingBlock( {object, ind, stop in
+            
+            var pId = object[playerIDKey] as String
+            
+            if(pId == playerId) {
+                index = ind
+                stop.initialize(true)
+            }
+            
+            
+        })
+        return index
+    }
+    
+    func setCurrentPlayerIndex(index : Int) {
+        
+    }
+    
+    func tryStartGame() {
+        if(_isPlayer1 && _gameState == GameState.kGameStateWaitingForStart) {
+            _gameState = GameState.kGameStateActive
+            self.sendGameBegin()
+            self.delegate?.setCurrentPlayerIndex(0)
+        }
+        
+    }
+    
+    func matchStarted() {
+        println("Match has started successfully")
+        if(_receivedAllRandomNumbers) {
+            _gameState = GameState.kGameStateWaitingForStart
+        }
+        else {
+            _gameState = GameState.kGameStateWaitingForRandomNumber;
+
+        }
+        self.sendRandomNumber()
+
+        self.tryStartGame()
+
+    }
+
+    func matchEnded() {
+        println("Match has ended")
+        delegate?.matchEnded()
+    }
+    
+    func match(match:GKMatch, didReceiveData data: NSData!, fromPlayer playerID: NSString!) {
+        
+        let message = UnsafePointer<Message>(data.bytes).memory
+
+        if(message.messageType == MessageType.kMessageTypeRandomNumber) {
+            let messageRandomNumber = UnsafePointer<MessageRandomNumber>(data.bytes).memory
+            
+            println("Received random number: \(messageRandomNumber.randomNumber)")
+            
+            var tie = false
+            
+            if(messageRandomNumber.randomNumber == _ourRandomNumber) {
+                println("Tie")
+                tie = true
+                _ourRandomNumber = arc4random()
+                self.sendRandomNumber()
+            }
+            else {
+                var dictionary = ["\(playerIDKey)":"\(playerID)", "\(randomNumberKey)":"\(messageRandomNumber.randomNumber)"]
+                self.processReceivedRandomNumber(dictionary)
+            }
+            
+            if(_receivedAllRandomNumbers) {
+                _isPlayer1 = self.isLocalPlayerPlayer1()
+            }
+            
+            if(!tie && _receivedAllRandomNumbers) {
+                if(_gameState == GameState.kGameStateWaitingForRandomNumber) {
+                    _gameState = GameState.kGameStateWaitingForStart
+                }
+                self.tryStartGame()
+            }
+        }
+        else if(message.messageType == MessageType.kMessageTypeGameBegin) {
+            println("Begin game message received")
+            _gameState = GameState.kGameStateActive
+            self.delegate?.setCurrentPlayerIndex(self.indexForLocalPlayer())
+        }
+        else if(message.messageType == MessageType.kMessageTypeMove) {
+            println("Move message received")
+            let messageMove = UnsafePointer<MessageMove>(data.bytes).memory
+            self.delegate?.movePlayerAtIndex(self.indexForPlayerWithId(playerID))
+        }
+        else if(message.messageType == MessageType.kMessageTypeGameOver) {
+            println("Game over message received")
+            let messageGameOver = UnsafePointer<MessageGameOver>(data.bytes).memory
+            self.delegate?.gameOver(messageGameOver.player1Won)
+        }
+        
+        //(message as Message).messageType
+        
+    }
+    
+    func processReceivedRandomNumber(randomNumberDetails:NSDictionary) {
+        
+        if(_orderOfPlayers.containsObject(randomNumberDetails)) {
+            _orderOfPlayers.removeObjectAtIndex(_orderOfPlayers.indexOfObject(randomNumberDetails))
+        }
+        _orderOfPlayers.addObject(randomNumberDetails)
+        
+        var sortByRandomNumber = NSSortDescriptor(key:randomNumberKey, ascending: false)
+        var sortDescriptors = [sortByRandomNumber]
+        _orderOfPlayers.sortUsingDescriptors(sortDescriptors)
+        
+        if(self.allRandomNumbersAreReceived()) {
+            _receivedAllRandomNumbers = true
+        }
+    }
+    
+    func allRandomNumbersAreReceived() -> Bool {
+        var receivedRandomNumbers = NSMutableArray()
+        
+        for dict in _orderOfPlayers {
+            receivedRandomNumbers.addObject(dict[randomNumberKey] as String)
+        }
+        
+        var set = NSSet()
+        
+        var arrayOfUniqueRandomNumbers = set.setByAddingObjectsFromArray(receivedRandomNumbers).allObjects
+        
+        if(arrayOfUniqueRandomNumbers.count == GameKitHelper.SharedGameKitHelper._match.playerIDs.count + 1) {
+            return true
+        }
+        return false
+    }
+    
+    func isLocalPlayerPlayer1() -> Bool {
+        var dictionary = _orderOfPlayers[0] as NSDictionary
+        if((dictionary[playerIDKey] as String) ==  (GKLocalPlayer.localPlayer().playerID)) {
+            println("I am player 1")
+            return true
+        }
+        return false
+    }
+}
